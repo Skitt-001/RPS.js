552582e894e814d6f5ddc82ca6f3ea124a3d25a1
Skitt-001 <190195566+Skitt-001@users.noreply.github.com>
2024-11-30 20:05:25 +0200

    commit

--- index.js
+++ index.js
@@ -1,60 +1,59 @@
 const Options = ["rock", "paper", "scissors"];
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorsBtn = document.getElementById("scissors");
+
+rockBtn.addEventListener("click", () => {
+    handleUserChoice("rock");
+
+});
+
+paperBtn.addEventListener("click", () => {
+    handleUserChoice("paper");
+
+});
+
+scissorsBtn.addEventListener("click", () => {
+    handleUserChoice("scissors");
+
+});
+
 function getComputerChoice(){
     return Options[Math.floor(Math.random() * Options.length)];
 }
-let times = Number(prompt("How many times do you want to play the game?"));
 
-let score = 0;
-let computerscore = 0;
-
-for (let i = 1; i <= times; i++){
-    let getUserChoice = prompt("Enter rock, paper or scissors.").toLowerCase();
+let handleUserChoice = (choice) => {
+    let computertext = document.getElementById("computerchoice");
+    let result = document.getElementById("result");
     let ComputerChoice = getComputerChoice();
 
-    if (ComputerChoice === "rock" && getUserChoice === "paper"){
-        console.log("You won!");
-        console.log(`The computer chose ${ComputerChoice}!`);
-        score += 1;
+    if (ComputerChoice === "rock" && choice === "paper"){
+        computertext.innerHTML = ComputerChoice;
+        result.innerHTML = "You won!"
     }
-    else if (ComputerChoice === "paper" && getUserChoice === "rock"){
-        console.log("You lost!");
-        console.log(`The computer chose ${ComputerChoice}!`);
-        computerscore += 1;
+    else if (ComputerChoice === "paper" && choice === "rock"){
+        computertext.innerHTML = ComputerChoice;
+        result.innerHTML = "You lost!!"
     }
-    else if (ComputerChoice === "paper" && getUserChoice === "scissors"){
-        console.log("You won!");
-        console.log(`The computer chose ${ComputerChoice}!`);
-        score += 1;
+    else if (ComputerChoice === "paper" && choice === "scissors"){
+        computertext.innerHTML = ComputerChoice;
+        result.innerHTML = "You won!"
     }
-    else if (ComputerChoice === "scissors" && getUserChoice === "paper"){
-        console.log("You lost!");
-        console.log(`The computer chose ${ComputerChoice}!`);
-        computerscore += 1;
+    else if (ComputerChoice === "scissors" && choice === "paper"){
+        computertext.innerHTML = ComputerChoice;
+        result.innerHTML = "You lost!"
     }
-    else if (ComputerChoice === "rock" && getUserChoice === "scissors"){
-        console.log("You lost!")
-        console.log(`The computer chose ${ComputerChoice}!`)
-        computerscore += 1;
+    else if (ComputerChoice === "rock" && choice === "scissors"){
+        computertext.innerHTML = ComputerChoice;
+        result.innerHTML = "You lost!"
     }
-    else if (ComputerChoice === "scissors" && getUserChoice === "rock"){
-        console.log("You won!");
-        console.log(`The computer chose ${ComputerChoice}!`);
-        score += 1;
+    else if (ComputerChoice === "scissors" && choice === "rock"){
+        computertext.innerHTML = ComputerChoice;
+        result.innerHTML = "You won!"
     }
     else {
-        console.log("Tie!");
-        console.log(`The computer also chose ${ComputerChoice}!`);
+        computertext.innerHTML = ComputerChoice;
+        result.innerHTML = "It's a Tie!"
     }
-} 
 
-if (score > computerscore){
-    console.log(`Congrats! You won! Your score is ${score} out of ${times}!`);
-}
-else if (score < computerscore){
-    console.log(`Sorry, You lost! Your score is ${score} out of ${times}!`);
-    console.log(`Computers score is ${computerscore} out of ${times}!)`);
-}
-else {
-    console.log(`It's a tie! Your score is ${score} out of ${times}!`);
-    console.log(`Computers score is ${computerscore} out of ${times}!`);
 }
